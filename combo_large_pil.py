445b78cb120ad64974fd4c1aa132cbd0b23bb5aa
teutoburg <ghost@instruct.at>
2023-01-05 15:51:54 +0100

    logging refactored, some cleanup etc.

--- combo_large_pil.py
+++ combo_large_pil.py
@@ -21,6 +21,15 @@ def _gma(i, g):
     return np.power(i, 1/g)
 
 
+def _pretty_info_log(msg_key):
+    msg_dir = {"single": "Start RGB processing for single Image...",
+               "multiple": "Start RGB processing for multiple Images...",
+               "done": "RGB processing done"}
+    logger.info("****************************************")
+    logger.info(msg_dir.get(msg_key, "Unknown log message."))
+    logger.info("****************************************")
+
+
 def create_picture(image_name, input_path, bands, n_bands, multi=False):
     new_pic = RGBPicture(name=image_name)
     if multi:
@@ -85,11 +94,9 @@ def create_rgb_image(input_path, output_path, image_name,
     logger.info("Image %s fully completed.", pic.name)
 
 
-def setup_rgb(input_path, output_path, image_name,
-              config_name=None, bands_name=None):
-    logger.info("****************************************")
-    logger.info("Start RGB processing...")
-    logger.info("****************************************")
+def setup_rgb_single(input_path, output_path, image_name,
+                     config_name=None, bands_name=None):
+    _pretty_info_log("single")
 
     if config_name is None:
         config_name = "./config.yml"
@@ -104,9 +111,34 @@ def setup_rgb(input_path, output_path, image_name,
     create_rgb_image(input_path, output_path, image_name, config, bands,
                      channel_combos)
 
-    logger.info("****************************************")
-    logger.info("RGB processing done")
-    logger.info("****************************************\n")
+    _pretty_info_log("done")
+
+
+def setup_rgb_multiple(input_path, output_path, image_names,
+                       config_name=None, bands_name=None,
+                       create_outfolder=False):
+    _pretty_info_log("multiple")
+
+    if config_name is None:
+        config_name = "./config.yml"
+    with open(config_name, "r") as ymlfile:
+        config = yaml.load(ymlfile, yaml.SafeLoader)
+
+    if bands_name is None:
+        bands_name = "./bands.yml"
+    bands = get_bands(bands_name, config)
+    channel_combos = config["combinations"]
+
+    for image_name in image_names:
+        if create_outfolder:
+            imgpath = output_path/image_name
+            imgpath.mkdir(parents=True, exist_ok=True)
+        else:
+            imgpath = output_path
+        create_rgb_image(input_path, imgpath, image_name, config, bands,
+                         channel_combos)
+
+    _pretty_info_log("RGB processing done")
 
 
 def _logging_configurator():
@@ -138,17 +170,14 @@ if __name__ == "__main__":
     target = "HOPS_99"
     # https://note.nkmk.me/en/python-pillow-concat-images/
 
-    setup_rgb(path, imgpath, target)
+    setup_rgb_single(path, imgpath, target)
 
     # root = Path("D:/Nemesis/data/perseus")
     # path = root/"stamps/"
 
     # for i in range(1, 4):
     #     target = f"IC 348-{i}"
-    #     imgpath = root/"RGBs"/target
-    #     imgpath.mkdir(parents=True, exist_ok=True)
-    
-    #     setup_rgb(path, imgpath, target)
+    #     setup_rgb(path, root/"RGBs", target)
 
     gc.collect()
     sys.exit(0)
