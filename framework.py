88420a050de7dd794b0484c57ad9da17b1e58677
teutoburg <ghost@instruct.at>
2023-01-05 14:20:48 +0100

    moved anything to do with source extraction to new SourcesFrame subclass

--- framework.py
+++ framework.py
@@ -88,9 +88,6 @@ class Frame():
 
         self.coords = wcs.WCS(self.header)
 
-        # self.stars = list()
-        # self.coord = None
-
     def __repr__(self):
         return str(self)  # DEBUG only
 
@@ -197,62 +194,6 @@ class Frame():
             self.image = np.nan_to_num(self.image, False, nan=med)
         else:
             raise ValueError("nanmode not understood")
-
-    def phot_zero(self):
-        """Calculate global photometrical zeropoint."""
-        for star in self.stars:
-            star.mk_phot_zeropoint(EXPTIME, EXTINCT, AIRMASS)
-
-        zeropoints = [star.phzp.get(self._band, None) for star in self.stars]
-        zeropoint = np.mean([zero.mag for zero in zeropoints if zero is not None])
-        err_zero = np.max([zero.err_mag for zero in zeropoints if zero is not None])
-        # self.phot_zero = CatFlux(self._band, zeropoint, err_zero)
-        return CatFlux(self._band, zeropoint, err_zero)
-
-    def add_cal_mag(self):
-        """legacy, check!"""
-        for star in self.stars:
-            star.cal_mag(self.phot_zero(), EXPTIME, EXTINCT, AIRMASS)
-
-    def _match_wcs_ref(self):
-        for star in self.stars:
-            if star.name != "n/a":
-                yield (star.wcs_ra, star.center["x"][0],
-                       star.wcs_de, star.center["y"][0])
-
-    def mk_wcs(self):
-        """create coordinate system"""
-        ra_cat, ra_px, de_cat, de_px = zip(*self._match_wcs_ref())
-
-        plt.scatter(ra_cat, ra_px)
-        plt.scatter(de_cat, de_px)
-
-        delt_ra, ref_ra, _, _, err_ra = sta.linregress(ra_px, ra_cat)
-        delt_de, ref_de, _, _, err_de = sta.linregress(de_px, de_cat)
-
-        print(f"RA {ref_ra:0.6f} +/- {err_ra:0.6f}")
-        print(f"DE {ref_de:0.6f} +/- {err_de:0.6f}")
-
-        self.coord = wcs.WCS(naxis=2)
-        self.coord.wcs.crpix = [ra_px[2], de_px[2]]
-        self.coord.wcs.cdelt = [delt_ra, delt_de]
-        self.coord.wcs.crval = [ra_cat[2], de_cat[2]]
-        self.coord.wcs.ctype = ["RA---AIR", "DEC--AIR"]
-
-        return self.coord
-
-    def display_all(self):
-        """Display image and all known stars.
-        Uses coordinate system if available, otherwise axes are in pixel."""
-        if self.coord is not None:
-            plt.subplot(projection=self.coord)
-        plt.imshow(self.image, vmin=100., vmax=1000., origin="lower")
-        plt.grid(color="white", ls="dashed")
-        plt.xlabel("RA")
-        plt.ylabel("DE")
-        plt.tight_layout()
-        for star in self.stars:
-            star.display()
 
     def display_3d(self):
         xx, yy = np.mgrid[0:self.image.shape[0], 0:self.image.shape[1]]
@@ -397,6 +338,76 @@ class Frame():
 
     def auto_gma(self):
         return np.exp((1 - (self.clipped_median + self.clipped_stddev)) / 2)
+
+
+class SourcesFrame(Frame):
+    """Subclass of Frame for use with source extraction."""
+
+    EXPTIME = 0.
+    EXTINCT = 0.
+    AIRMASS = 0.
+
+    def __init__(self, image, band, header=None, **kwargs):
+        super().__init__(image, band, header, **kwargs)
+        self.stars = list()
+        self.coord = None
+
+    def phot_zero(self):
+        """Calculate global photometrical zeropoint."""
+        for star in self.stars:
+            star.mk_phot_zeropoint(self.EXPTIME, self.EXTINCT, self.AIRMASS)
+
+        zeropoints = [star.phzp.get(self._band, None) for star in self.stars]
+        zeropoint = np.mean([zero.mag for zero in zeropoints if zero is not None])
+        err_zero = np.max([zero.err_mag for zero in zeropoints if zero is not None])
+        # self.phot_zero = CatFlux(self._band, zeropoint, err_zero)
+        return CatFlux(self._band, zeropoint, err_zero)
+
+    def add_cal_mag(self):
+        """legacy, check!"""
+        for star in self.stars:
+            star.cal_mag(self.phot_zero(), self.EXPTIME, self.EXTINCT,
+                         self.AIRMASS)
+
+    def _match_wcs_ref(self):
+        for star in self.stars:
+            if star.name != "n/a":
+                yield (star.wcs_ra, star.center["x"][0],
+                       star.wcs_de, star.center["y"][0])
+
+    def mk_wcs(self):
+        """create coordinate system"""
+        ra_cat, ra_px, de_cat, de_px = zip(*self._match_wcs_ref())
+
+        plt.scatter(ra_cat, ra_px)
+        plt.scatter(de_cat, de_px)
+
+        delt_ra, ref_ra, _, _, err_ra = sta.linregress(ra_px, ra_cat)
+        delt_de, ref_de, _, _, err_de = sta.linregress(de_px, de_cat)
+
+        print(f"RA {ref_ra:0.6f} +/- {err_ra:0.6f}")
+        print(f"DE {ref_de:0.6f} +/- {err_de:0.6f}")
+
+        self.coord = wcs.WCS(naxis=2)
+        self.coord.wcs.crpix = [ra_px[2], de_px[2]]
+        self.coord.wcs.cdelt = [delt_ra, delt_de]
+        self.coord.wcs.crval = [ra_cat[2], de_cat[2]]
+        self.coord.wcs.ctype = ["RA---AIR", "DEC--AIR"]
+
+        return self.coord
+
+    def display_all(self):
+        """Display image and all known stars.
+        Uses coordinate system if available, otherwise axes are in pixel."""
+        if self.coord is not None:
+            plt.subplot(projection=self.coord)
+        plt.imshow(self.image, vmin=100., vmax=1000., origin="lower")
+        plt.grid(color="white", ls="dashed")
+        plt.xlabel("RA")
+        plt.ylabel("DE")
+        plt.tight_layout()
+        for star in self.stars:
+            star.display()
 
 
 class Picture():
