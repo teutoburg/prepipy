32534c0639d376a341c78d4ab45592b75962142e
teutoburg <ghost@instruct.at>
2023-03-12 18:22:29 +0100

    added default values and types

--- configuration.py
+++ configuration.py
@@ -8,46 +8,49 @@ Created on Sun Mar 12 17:48:17 2023
 @author: teuto
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Union
-
-@dataclass
-class Configurator:
-    general
-    process
-    figures
-    use_bands: list[str]
-    combinations: list[list[str]]
 
 
 @dataclass
 class GeneralConfigurator:
-    filenames: ${band_name}_${image_name}.fits
+    filenames: str = "${band_name}_${image_name}.fits"
     # e.g. $band_name/fits/$image_name.fits or ${image_name}_${band_name}.fits
-    multiprocess: no      # no (default) or yes
-    jpeg_quality: 95      # see pillow documentation for details
+    multiprocess: bool = False  # no (default) or yes
+    jpeg_quality: int = 95      # see pillow documentation for details
 
 
 @dataclass
 class ProcessConfigurator:
-    grey_mode: normal     # normal, lessback or moreback
-    rgb_adjust: no        # no (default) or yes
-    alpha: 1.             # float (default=1.), typ. 1-2
-    gamma_lum: 1.         # float (default=1.), typ. 1-3
-    clip: 10.             # float (default=10.)
-    nanmode: max          # max (default) or median
-    skymode: median       # median (default), clipmedian, quantile or debug
-    maxmode: quantile     # quantile (default), max or debug
-    slice: NULL           # NULL (default) or [wx, wy]
-    equal_offset: .1      # float (default=.1)
-    equal_norm: yes       # yes (default) or no
+    grey_mode: str = "normal"  # normal, lessback or moreback
+    rgb_adjust: bool = False   # no (default) or yes
+    alpha: float = 1.          # float (default=1.), typ. 1-2
+    gamma_lum: float = 1.      # float (default=1.), typ. 1-3
+    clip: float = 10.          # float (default=10.)
+    nanmode: str = "max"       # max (default) or median
+    skymode: str = "median"    # median (default), clipmedian, quantile or debug
+    maxmode: str = "quantile"  # quantile (default), max or debug
+    slice: Union[list[int], None] = None  # NULL (default) or [wx, wy]
+    equal_offset: float = .1   # float (default=.1)
+    equal_norm: bool = True    # yes (default) or no
 
 @dataclass
 class FiguresConfigurator:
-    titlemode: debug      # debug or pub
-    include_suptitle: yes # yes (default) or no
-    figsize: [3, 5.6]     # [col, row]
-    max_cols: 4           # int (default=4)
-    centermark: no        # no (default) or yes
-    gridlines: no         # no (default) or yes
-    additional_roi:       # each as [ra, dec] in decimal deg format
+    titlemode: str = "debug"       # debug or pub
+    include_suptitle: bool = True  # yes (default) or no
+    figsize: list[int] = field(default_factory=lambda: [3, 5.6])
+    # [col, row]
+    max_cols: int = 4              # int (default=4)
+    centermark: bool = False       # no (default) or yes
+    gridlines: bool = False        # no (default) or yes
+    additional_roi: Union[list[list[int]], None] = None
+    # each as [ra, dec] in decimal deg format
+
+
+@dataclass
+class Configurator:
+    general: GeneralConfigurator
+    process: ProcessConfigurator
+    figures: FiguresConfigurator
+    use_bands: list[str] = field(default_factory=list)
+    combinations: list[list[str]] = field(default_factory=list)
